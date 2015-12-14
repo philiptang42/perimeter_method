a797c51554358841ecc450aabe3a2380d08957c1
philiptang42 <philiptang42@gmail.com>
2015-12-13 19:16:02 -0500

    part 2 solution

--- perimeter_method.rb
+++ perimeter_method.rb
@@ -1,5 +1,9 @@
-def perimeter(width, height)
-  (width * 2) + (height * 2)
+def perimeter(width, height = nil)
+  if height == nil
+    width * 4
+  else
+    (width * 2) + (height * 2)
+  end
 end
 
-puts perimeter(2, 2)
+puts perimeter(5)
